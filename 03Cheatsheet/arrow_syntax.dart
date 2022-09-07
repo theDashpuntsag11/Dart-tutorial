da191cd673c8e32a0ca8804c8b30d9774e2ff247
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-09-07 15:55:26 +0800

    day2 change5

---
+++ 03Cheatsheet/arrow_syntax.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+/**
+ *  Arrow syntax is a way to define a function that executes the expression to its right and returns its value.
+ */
+void main(List<String> args) {
+  final aListOfStrings = ['one', 'two', 'three'];
+  final bListOfStrings = ['one', '', 'three'];
+
+  bool hasEmpty = aListOfStrings.any((element) => element.isEmpty);
+  print('hasEmpty is: $hasEmpty');
+
+  hasEmpty = bListOfStrings.any((element) => element.isEmpty);
+  print('hasEmpty after is: $hasEmpty');
+
+  final intList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
