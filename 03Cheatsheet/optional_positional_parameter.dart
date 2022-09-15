1ffdbbc80f52ca41779a89f829f9926cfcaefe98
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-09-15 22:32:42 +0800

    day6 part2

--- 03Cheatsheet/optional_positional_parameter.dart
+++ 03Cheatsheet/optional_positional_parameter.dart
@@ -1,7 +1,7 @@
 /**
  *      Dart has 2 kinds of function parameters: 
  *          - postional ()
- *          - named
+ *          - named ()
  * 
  */
 
@@ -10,7 +10,20 @@ int sumUp(int a, int b, int c) {
   return a + b + c;
 }
 
+// This is function with optional positional parameters
+// Optional positional parameters are always last in a function's parametr list
+// Their initial value is null, Unless you provide a another default value
+int sumUpToFive(int a, [int? b, int? c, int? d, int? e]) {
+  int sum = a;
+  sum += b ?? 0;
+  sum += c ?? 0;
+  sum += d ?? 0;
+  sum += e ?? 0;
+  return sum;
+}
+
 void main(List<String> args) {
   print("hello");
   print('sumUp() = ' + sumUp(1, 2, 3).toString());
+  print('sumUpToFive = ' + sumUpToFive(1, 2, 3, 4, 5).toString());
 }
