53e2aed8511c11576e343c29cdaac714ab0eb473
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-09-17 23:59:21 +0800

    Day8 part2

--- 03Cheatsheet/exeptions.dart
+++ 03Cheatsheet/exeptions.dart
@@ -1,3 +1,14 @@
+/** 
+ *  Dart code can throw and catch exceptions. 
+ *  In contrast to Java, all of Dart’s exceptions are unchecked exceptions. 
+ *  Methods don’t declare which exceptions they might throw, and you aren’t required to catch any exceptions.
+ * 
+ */
+
 void main(List<String> args) {
+  // Dart provides Exception and Error types, but you’re allowed to throw any non-null object:
+  throw Exception('Something happened');
+  throw 'Waah!';
+
   print('hello');
 }
