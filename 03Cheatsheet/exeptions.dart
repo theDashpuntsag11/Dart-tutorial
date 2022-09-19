15d641bcc27a95ac582d5ba24c6c0c68315a96ef
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-09-19 23:11:28 +0800

    day9 part1

--- 03Cheatsheet/exeptions.dart
+++ 03Cheatsheet/exeptions.dart
@@ -7,8 +7,25 @@
 
 void main(List<String> args) {
   // Dart provides Exception and Error types, but you’re allowed to throw any non-null object:
-  throw Exception('Something happened');
-  throw 'Waah!';
+  // throw Exception('Something happened');
+  // throw 'Waah!';
+
+  //  The try keyword works as it does in most other languages.
+  //  Use the on keyword to filter for specific exceptions by type, and the catch keyword to get a reference to the exception object.
+  //  If you can’t completely handle the exception, use the rethrow keyword to propagate the exception:
+  try {
+    breedMoreLlamas();
+  } on OutOfLlamasException {
+    // A specific exception
+    buyMoreLlamas();
+  } on Exception catch (e) {
+    // Anything else that is an exception
+    print('Unknown exception: $e');
+  } catch (e) {
+    // No specified type, handles all
+    print('Something really unknown: $e');
+    rethrow;
+  }
 
   print('hello');
 }
